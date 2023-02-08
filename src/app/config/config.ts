56198e6b729a285fa89cb9877f2a8a8547eddbcd
Francortiz-137 <francortiz.c@gmail.com>
2023-02-08 15:38:52 -0300

    fixed: show profile pics

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,2 @@
-export const URL_BACKEND = 'https://facturacion-app-production.up.railway.app';
+//export const URL_BACKEND = 'https://facturacion-app-production.up.railway.app';
+export const URL_BACKEND = 'http://localhost:8080';
